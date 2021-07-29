b1016999f77b5a9490533c0890b2abef6265ab07
ssaniya-dev <ssaniyasavla@gmail.com>
2021-07-29 17:48:06 -0400

    Adds two functions to AppComponent and adds unit tests.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,65 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+// import { TestBed } from '@angular/core/testing';
+// import { RouterTestingModule } from '@angular/router/testing';
+// import { AppComponent } from './app.component';
+
+// describe('AppComponent', () => {
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [
+//         RouterTestingModule
+//       ],
+//       declarations: [
+//         AppComponent
+//       ],
+//     }).compileComponents();
+//   });
+
+//   it('should create the app', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.componentInstance;
+//     expect(app).toBeTruthy();
+//   });
+
+//   it(`should have as title 'calculator'`, () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.componentInstance;
+//     expect(app.title).toEqual('calculator');
+//   });
+
+//   it('should render title', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     fixture.detectChanges();
+//     const compiled = fixture.nativeElement;
+//     expect(compiled.querySelector('.content span').textContent).toContain('calculator app is running!');
+//   });
+// });
+
+import { AppComponent } from "./app.component"
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+  let systemUnderTest: AppComponent;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  beforeEach(() => {
+    systemUnderTest = new AppComponent();
+  })
 
-  it(`should have as title 'calculator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('calculator');
-  });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('calculator app is running!');
-  });
-});
+  it('should return a primary button given a number in getClass', () => {
+    const num = '7'
+    const result = systemUnderTest.getClass(num);
+    expect(result).toEqual('btn btn-primary');
+  })
+
+  it('should return success button when given a non-number is given in getClass', () => {
+    const nonNum = '+'
+    const result = systemUnderTest.getClass(nonNum);
+    expect(result).toEqual('btn btn-success');
+  })
+
+  it('should divide two numbers', () => {
+    const num1 = 5;
+    const num2 = 2;
+    const result = systemUnderTest.divideTwoNumbers(num1, num2)
+    expect(result).toEqual(2.5)
+  })
+})
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,40 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'calculator';
+  rowFormat: string[][];
+
+  constructor() {
+    this.rowFormat = [
+      ['1', '2', '3', '+'],
+      ['4', '5', '6', '-'],
+      ['7', '8', '9', '*'],
+      ['.', '0', '=', '/']
+    ];
+  }
+
+  getClass(str:string) {
+    if (str >= '0' && str <= '9') {
+      return 'btn btn-primary';
+    } else {
+      return 'btn btn-success';
+    }
+  }
+  addTwoNumbers(num1: any, num2: any) {
+    if(num1 === null && num2 === null) {
+      return 0 
+    }
+    const add = num1+num2;
+    return add;
+  }
+
+  divideTwoNumbers(num1: any, num2: any) {
+    if(num2 === 0) {
+      return undefined;
+    }
+    const divide = num1/ num2;
+    return divide
+  }
 }
